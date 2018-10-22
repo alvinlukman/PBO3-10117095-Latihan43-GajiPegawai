2b0eaaffdaf486ab3b859aed3c66bf864562f4de
alvinlukmanNH <galinx20gmail.com>
2018-10-22 23:10:09 +0700

    gajipagawai

---
+++ src/Gaji.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  :Alvin Lukman Nulhakim
+ * NIM   :10117095    
+ * KELAS :IF-3
+ */
+public class Gaji {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+    
+}
